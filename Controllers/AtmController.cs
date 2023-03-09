4d0f4bd2dd984f8b1ea0426f680caaf88a497dd2
stantheguru <sirstanleyzurki@gmail.com>
2023-03-09 08:10:57 +0300

    updates

--- Controllers/AtmController.cs
+++ Controllers/AtmController.cs
@@ -72,13 +72,7 @@ namespace atm.Controllers
                 cmd.Parameters.Add(new NpgsqlParameter("@ActualBalance", customer.ActualBalance));
                 cmd.Parameters.Add(new NpgsqlParameter("@RegistrationDate", DateTime.Now));
 
-                string queryString = cmd.CommandText;
-                foreach (NpgsqlParameter param in cmd.Parameters)
-                {
-                    queryString = queryString.Replace(param.ParameterName, param.Value.ToString());
-                }
-
-                Console.WriteLine("Executing SQL query: " + queryString);
+                
 
                 conn.Open();
                 int noOfRowsAffected = cmd.ExecuteNonQuery();
@@ -129,7 +123,7 @@ namespace atm.Controllers
         [HttpPost]
         public async Task<string> Login([FromForm] Login user)
         {
-            string sqlExists = "select CustomerID, Email, PIN, AccountName, MobileNumber, WithdrawalLimit from dbo.Customers where Email='" + user.Email + "'";
+            string sqlExists = "select customer_id, email, pin, account_name, mobile_number, withdrawal_limit from customers where email='" + user.Email + "'";
             
             NpgsqlCommand NpgsqlCommand = new NpgsqlCommand(sqlExists, conn);
             conn.Open();
@@ -138,11 +132,11 @@ namespace atm.Controllers
             JObject job = new JObject();
             while (reader.Read())
             {
-                var CustomerID = reader["CustomerID"].ToString();
-                var PIN = reader["PIN"].ToString();
-                var mobile = reader["MobileNumber"].ToString();
-                var Name = reader["AccountName"].ToString();
-                var WithdrawalLimit = reader["WithdrawalLimit"].ToString();
+                var CustomerID = reader["customer_id"].ToString();
+                var PIN = reader["pin"].ToString();
+                var mobile = reader["mobile_number"].ToString();
+                var Name = reader["account_name"].ToString();
+                var WithdrawalLimit = reader["withdrawal_limit"].ToString();
 
                 job.Add("MobileNumber", mobile);
                 job.Add("AccountName", Name);
@@ -202,15 +196,15 @@ namespace atm.Controllers
             var AvailableBalance = 0.0f;
             try
             {
-                string sqlBalance = "select ActualBalance, AvailableBalance from dbo.Customers where CustomerID='" + withdrawal.CustomerID + "'";
+                string sqlBalance = "select actual_balance, available_balance from customers where customer_id='" + withdrawal.CustomerID + "'";
                 NpgsqlCommand NpgsqlCommand = new NpgsqlCommand(sqlBalance, conn);
                 conn.Open();
                 NpgsqlDataReader reader = NpgsqlCommand.ExecuteReader();
                 int noOfRowsAffected = 0;
                 while (reader.Read())
                 {
-                    Balance = float.Parse(reader["ActualBalance"].ToString());
-                    AvailableBalance = float.Parse(reader["AvailableBalance"].ToString());
+                    Balance = float.Parse(reader["actual_balance"].ToString());
+                    AvailableBalance = float.Parse(reader["available_balance"].ToString());
                     
                     
                 }
@@ -221,11 +215,13 @@ namespace atm.Controllers
                     
                     conn.Open();
                    
-                    string query = "insert into Transactions values(@TransactionTypeID, @TransactionAmount, @TransactionDate)";
+                    string query = "insert into transactions (transaction_type_id, transaction_amount, transaction_date) values(@TransactionTypeID, @TransactionAmount, @TransactionDate)";
                     NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
                     cmd.Parameters.Add(new NpgsqlParameter("@TransactionTypeID", 1));
                     cmd.Parameters.Add(new NpgsqlParameter("@TransactionAmount", withdrawal.Amount));
                     cmd.Parameters.Add(new NpgsqlParameter("@TransactionDate", DateTime.Now));
+
+                    
 
 
                     noOfRowsAffected = cmd.ExecuteNonQuery();
@@ -244,7 +240,7 @@ namespace atm.Controllers
                 var TransactionID = "";
                 if (noOfRowsAffected > 0)
                 {
-                    string sqlTransaction = "select MAX(TransactionID) as id from dbo.Transactions";
+                    string sqlTransaction = "select MAX(transaction_id) as id from transactions";
                     NpgsqlCommand sqlId = new NpgsqlCommand(sqlTransaction, conn);
                     conn.Open();
                     NpgsqlDataReader sqlDataReader = sqlId.ExecuteReader();
@@ -258,12 +254,14 @@ namespace atm.Controllers
                     conn.Close();
 
                     //Save withdrawal
-                    string query = "insert into Withdrawals values(@TransactionID,@CustomerID, @Amount, @WithdrawalDate)";
+                    string query = "insert into withdrawals(transaction_id, customer_id, amount, withdrawal_date) values(@TransactionID,@CustomerID, @Amount, @WithdrawalDate)";
                     NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
-                    cmd.Parameters.Add(new NpgsqlParameter("@TransactionID", TransactionID));
+                    cmd.Parameters.Add(new NpgsqlParameter("@TransactionID", int.Parse(TransactionID)));
                     cmd.Parameters.Add(new NpgsqlParameter("@CustomerID", withdrawal.CustomerID));
                     cmd.Parameters.Add(new NpgsqlParameter("@Amount", withdrawal.Amount));
                     cmd.Parameters.Add(new NpgsqlParameter("@WithdrawalDate", DateTime.Now));
+
+                    
 
                     conn.Open();
                     rowsChanged = cmd.ExecuteNonQuery();
@@ -273,7 +271,7 @@ namespace atm.Controllers
                 //Update balance
                 if (rowsChanged > 0)
                 {
-                    string sqUpdate = "UPDATE Customers SET ActualBalance=@ActualBalance, AvailableBalance=@AvailableBalance where CustomerID = @CustomerID";
+                    string sqUpdate = "UPDATE customers SET actual_balance=@ActualBalance, available_balance=@AvailableBalance where customer_id = @CustomerID";
                     NpgsqlCommand sq = new NpgsqlCommand(sqUpdate, conn);
 
                     sq.Parameters.Add(new NpgsqlParameter("@ActualBalance", Balance-withdrawal.Amount));
@@ -310,7 +308,7 @@ namespace atm.Controllers
         [HttpPost]
         public async Task<string> Balance([FromForm] Balance balance)
         {
-            string query = "select * from dbo.Customers where Email='" + balance.Email + "'";
+            string query = "select * from customers where email='" + balance.Email + "'";
 
             NpgsqlCommand NpgsqlCommand = new NpgsqlCommand(query, conn);
             conn.Open();
@@ -319,10 +317,10 @@ namespace atm.Controllers
             JObject job = new JObject();
             while (reader.Read())
             {
-                var AccountName = reader["AccountName"].ToString().ToUpper();
-                var AccountNumber = reader["AccountNumber"].ToString();
-                var AvailableBalance = float.Parse(reader["AvailableBalance"].ToString());
-                var ActualBalance = float.Parse(reader["ActualBalance"].ToString());
+                var AccountName = reader["account_name"].ToString().ToUpper();
+                var AccountNumber = reader["account_number"].ToString();
+                var AvailableBalance = float.Parse(reader["available_balance"].ToString());
+                var ActualBalance = float.Parse(reader["actual_balance"].ToString());
                 
 
                 job.Add("AccountName", AccountName);
@@ -349,7 +347,7 @@ namespace atm.Controllers
             var AvailableBalance = 0.0f;
             try
             {
-                string sqlBalance = "select ActualBalance, AvailableBalance from dbo.Customers where Email='" + transfer.SenderEmail + "'";
+                string sqlBalance = "select actual_balance, available_balance from customers where email='" + transfer.SenderEmail + "'";
                 NpgsqlCommand NpgsqlCommand = new NpgsqlCommand(sqlBalance, conn);
                 conn.Open();
                 Console.WriteLine(sqlBalance);
@@ -357,8 +355,8 @@ namespace atm.Controllers
                 int noOfRowsAffected = 0;
                 while (reader.Read())
                 {
-                    Balance = float.Parse(reader["ActualBalance"].ToString());
-                    AvailableBalance = float.Parse(reader["AvailableBalance"].ToString());
+                    Balance = float.Parse(reader["actual_balance"].ToString());
+                    AvailableBalance = float.Parse(reader["available_balance"].ToString());
                     Console.WriteLine(Balance);
                     Console.WriteLine(AvailableBalance);
 
@@ -372,7 +370,7 @@ namespace atm.Controllers
 
                     conn.Open();
 
-                    string query = "insert into Transactions values(@TransactionTypeID, @TransactionAmount, @TransactionDate)";
+                    string query = "insert into transactions(transaction_type_id, transaction_amount, transaction_date) values(@TransactionTypeID, @TransactionAmount, @TransactionDate)";
                     NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
                     cmd.Parameters.Add(new NpgsqlParameter("@TransactionTypeID", 2));
                     cmd.Parameters.Add(new NpgsqlParameter("@TransactionAmount", transfer.Amount));
@@ -396,7 +394,7 @@ namespace atm.Controllers
                 var TransactionID = "";
                 if (noOfRowsAffected > 0)
                 {
-                    string sqlTransaction = "select MAX(TransactionID) as id from dbo.Transactions";
+                    string sqlTransaction = "select MAX(transaction_id) as id from transactions";
                     NpgsqlCommand sqlId = new NpgsqlCommand(sqlTransaction, conn);
                     conn.Open();
                     NpgsqlDataReader sqlDataReader = sqlId.ExecuteReader();
@@ -412,9 +410,9 @@ namespace atm.Controllers
                     
 
                     //Save withdrawal
-                    string query = "insert into Transfers values(@TransactionID,@SenderEmail, @RecipientAccount, @Amount, @TransferDate)";
+                    string query = "insert into transfers(transaction_id,sender_email,recipient_account,amount,transfer_date) values(@TransactionID,@SenderEmail, @RecipientAccount, @Amount, @TransferDate)";
                     NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
-                    cmd.Parameters.Add(new NpgsqlParameter("@TransactionID", TransactionID));
+                    cmd.Parameters.Add(new NpgsqlParameter("@TransactionID", int.Parse(TransactionID)));
                     cmd.Parameters.Add(new NpgsqlParameter("@SenderEmail", transfer.SenderEmail));
                     cmd.Parameters.Add(new NpgsqlParameter("@RecipientAccount", transfer.RecipientAccount));
                     cmd.Parameters.Add(new NpgsqlParameter("@Amount", transfer.Amount));
@@ -429,7 +427,7 @@ namespace atm.Controllers
                 int senderRows = 0;
                 if (rowsChanged > 0)
                 {
-                    string sqUpdate = "UPDATE Customers SET ActualBalance=@ActualBalance, AvailableBalance=@AvailableBalance where Email = @SenderEmail";
+                    string sqUpdate = "UPDATE customers SET actual_balance=@ActualBalance, available_balance=@AvailableBalance where email = @SenderEmail";
                     NpgsqlCommand sq = new NpgsqlCommand(sqUpdate, conn);
 
                     sq.Parameters.Add(new NpgsqlParameter("@ActualBalance", Balance - transfer.Amount));
@@ -448,21 +446,21 @@ namespace atm.Controllers
                 var AccountName = "";
                 if (senderRows > 0)
                 {
-                    string sqlRecipient = "select ActualBalance, AvailableBalance, AccountName from dbo.Customers where AccountNumber = '"+transfer.RecipientAccount + "'";
+                    string sqlRecipient = "select actual_balance, available_balance, account_name from customers where account_number = '"+transfer.RecipientAccount + "'";
                     NpgsqlCommand recipientCommand = new NpgsqlCommand(sqlRecipient, conn);
                     conn.Open();
                     NpgsqlDataReader sqlDataReader = recipientCommand.ExecuteReader();
 
                     while (sqlDataReader.Read())
                     {
-                        ActualBalanceRecipient = float.Parse(sqlDataReader["ActualBalance"].ToString());
-                        AvailableBalanceRecipient = float.Parse(sqlDataReader["AvailableBalance"].ToString());
-                        AccountName = sqlDataReader["AccountName"].ToString().ToUpper();
+                        ActualBalanceRecipient = float.Parse(sqlDataReader["actual_balance"].ToString());
+                        AvailableBalanceRecipient = float.Parse(sqlDataReader["available_balance"].ToString());
+                        AccountName = sqlDataReader["account_name"].ToString().ToUpper();
 
                     }
                     conn.Close();
 
-                    string sqUpdate = "UPDATE Customers SET ActualBalance=@ActualBalance, AvailableBalance=@AvailableBalance where AccountNumber = @RecipientAccount";
+                    string sqUpdate = "UPDATE customers SET actual_balance=@ActualBalance, available_balance=@AvailableBalance where account_number = @RecipientAccount";
                     NpgsqlCommand sq = new NpgsqlCommand(sqUpdate, conn);
 
                     sq.Parameters.Add(new NpgsqlParameter("@ActualBalance", Balance + transfer.Amount));
